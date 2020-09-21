fe4e10b5a6f36bc9ce84f2086b74ba6fbee8b8fa
Joshua Macklin <69019114+JoshuaMacklin@users.noreply.github.com>
2020-09-21 00:11:25 -0700

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://test:test@cluster0.x4m5e.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demo'
+};
